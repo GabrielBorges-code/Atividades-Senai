c5dee2f601c78047a3470b09d51078db6fcf525b
Gabriel B <gabriel.moura2009@hotmail.com>
2020-09-18 18:22:36 -0300

    rename files

---
+++ Atividades_02/ex01.js
@@ -0,0 +1,14 @@
+👉Faça um programa que recebe um usuário e senha
+e valida a senha e o usuário com entrada de dados
+Dica(Tente simular um acesso estático e dinâmico usando funções e estruturas de decisão)
+
+👉Crie um chat para um cliente que quer comprar um carro com
+entrada de dados.
+Dica(Use estruturas de decisão e ou vetores ou funções...)
+
+👉Crie um programa que apresenta um cardápio ao cliente
+de pizzas diversas, com entrada de dados.
+Dica(Use estruturas de decisão e objetos ou vetores...)
+
+ 👉Opcional 
+Envie um arquivo de texto para o git usando terminal bash como na aula anterior.
