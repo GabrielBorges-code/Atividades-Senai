59582e6c2d93782ee48680c317edf247d4ad9e58
Gabriel B <gabriel.moura2009@hotmail.com>
2020-09-18 19:29:27 -0300

    cardapio

---
+++ Atividades_02/ex02.js
@@ -0,0 +1,3 @@
+// 👉Crie um chat para um cliente que quer comprar um carro com
+// entrada de dados.
+// Dica(Use estruturas de decisão e ou vetores ou funções...)
---
+++ Atividades_02/ex03.js
@@ -0,0 +1,15 @@
+// 👉Crie um programa que apresenta um cardápio ao cliente
+// de pizzas diversas, com entrada de dados.
+// Dica(Use estruturas de decisão e objetos ou vetores...)
+
+
+function cardapio(){
+    var pizzaTradicionais = ["Calabresa", "Baiana", "Frango Catupiry", "Marguerita", "Napolitana"]
+    var pizzaDoce = ["Banana com Canela", "Chocolate", "Sorverte", "Sensação", "Doce de leite"]
+    var pizzaPremiun = ["Búfala La Bianca","Corn & Bacon","De Luxe","Pepperrock"]
+
+    console.log(`Pizza de: ` )
+  
+}
+
+cardapio()
